6f76c4f15fae682277c75f3e7ce2e6e9e6426e3a
Miran <miranabdelaziz@yahoo.com>
2024-09-24 23:55:42 -0400

    -created Landing tsx and Game tsx  inside screens folder -created Button.tsx as a component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing  from './screens/Landing';
+import  Game  from './screens/Game';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <button>
-        Join
-      </button>
+      <div className='h-screen bg-slate-950'>
+     <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Landing />} /> 
+      <Route path="/game" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
     </>
   )
 }
